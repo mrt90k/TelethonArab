1d2cd097671ab2733b1d670e2a5b17495abed853
Telethon-Arabs <83516836+telethon-Arab@users.noreply.github.com>
2021-12-13 19:45:43 +0300

    @telethon-Arab
    @telethon-Arab

--- SQL/extras/Source.py
+++ SQL/extras/Source.py
@@ -8,6 +8,4 @@ help1 = ("**⎈ ⦙ كيفيه التنصيب :**")
 help2 = ("**⎈ ⦙ قـائمـه الاوامـر :**\n**⎈ ⦙ قنـاه السـورس :** @IQTHON\n**⎈ ⦙ شـرح اوامـر السـورس : @L3LL3**\n**⎈ ⦙ شـرح فـارات السـورس : @TEAMTELETHON** ")
 TG_BOT = Config.TG_BOT_USERNAME
 TM = time.strftime("%I:%M")
-ALIVE_TEXT = "𝗐𝖾𝗅𝖼𝗈𝗆𝖾 𝗍𝖾𝗅𝖾𝗍𝗁𝗈𝗇 𝖺𝗅 𝖺𝗋𝖺𝖻 𓃠"
-EMOJI = "⎈ ⦙"
-Sour = f"**{ALIVE_TEXT}**\n𓍹ⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧ𓍻\n**{EMOJI}  النسخـة :  ِ7.1 ** \n**{EMOJI} حسـابك  :   {mention} **\n**{EMOJI} الـوقت  : {TM} **\n**{EMOJI} البوت :** {TG_BOT}\n**{EMOJI} السـورس :** @IQTHON \n𓍹ⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧ𓍻\n"
+Sour = f"**.𓄌 : version 7.2  𓇡.** \n.𓄌 : me  {mention}  𓇡. \n**.𓄌 : time  {TM}  𓇡.**\n**.𓄌 : My Bot {TG_BOT} 𓇡.**\n**.𓄌 : Source TelethonArab : @iqthon  𓇡.**"
