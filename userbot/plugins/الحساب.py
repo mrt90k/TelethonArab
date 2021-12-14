39b84d6955898c87a58a3235ee8b49d571d22467
Telethon-Arabs <83516836+telethon-Arab@users.noreply.github.com>
2021-12-14 17:33:15 +0300

    Update الحساب.py

--- userbot/plugins/الحساب.py
+++ userbot/plugins/الحساب.py
@@ -870,7 +870,7 @@ async def autobio_loop():
     while AUTOBIOSTART:
         HM = time.strftime("%I:%M")
         Dont1Tags = gvarstatus("FONTS_AUTO") or "font1"
-        FONT1 = requests.get(f"https://telethon.ml/{Dont1Tags}.php?text={HM}").json()['newText']
+        FONT1 = requests.get(f"https://telethonch.ml/FONTS/{Dont1Tags}.php?text={HM}").json()['newText']
         bio = f"{EMOJI_TELETHON} {DEFAULTUSERBIO}  - {FONT1}"
         LOGS.info(bio)
         try:
@@ -933,7 +933,7 @@ async def autoname_loop():
     while AUTONAMESTART:
         HM = time.strftime("%I:%M")
         Dont1Tags = gvarstatus("FONTS_AUTO") or "font1"
-        FONT1 = requests.get(f"https://telethon.ml/{Dont1Tags}.php?text={HM}").json()['newText']
+        FONT1 = requests.get(f"https://telethonch.ml/FONTS/{Dont1Tags}.php?text={HM}").json()['newText']
         name = f"{EMOJI_TELETHON} {FONT1} | "
         LOGS.info(name)
         try:
