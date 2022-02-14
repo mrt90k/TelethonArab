f16d8696c25fe21b97eb798f73c3cc0a870e684a
telethonAr <96899227+telethonAr@users.noreply.github.com>
2022-02-14 09:48:43 +0300

    Update Source.py

--- SQL/extras/Source.py
+++ SQL/extras/Source.py
@@ -4,8 +4,8 @@ from datetime import datetime
 from userbot import StartTime, iqthon
 from userbot.Config import Config
 from userbot.plugins import mention
-help1 = ("**⎈ ⦙ كيفيه التنصيب :**")
-help2 = ("**⎈ ⦙ قـائمـه الاوامـر :**\n**⎈ ⦙ قنـاه السـورس :** @IQTHON\n**⎈ ⦙ شـرح اوامـر السـورس : @L3LL3**\n**⎈ ⦙ شـرح فـارات السـورس : @TEAMTELETHON** ")
+help1 = ("**☭⦙ كيفيه التنصيب :**")
+help2 = ("**☭⦙ قـائمـه الاوامـر :**\n**☭⦙ قنـاه السـورس :** @IQTHON\n**☭⦙ شـرح اوامـر السـورس : @L3LL3**\n**☭⦙ شـرح فـارات السـورس : @TEAMTELETHON** ")
 TG_BOT = Config.TG_BOT_USERNAME
 TM = time.strftime("%I:%M")
 Sour = f"**.𓄌 : version 7.5  𓇡.** \n.𓄌 : me  {mention}  𓇡. \n**.𓄌 : time  {TM}  𓇡.**\n**.𓄌 : My Bot {TG_BOT} 𓇡.**\n**.𓄌 : Source TelethonArab : @iqthon  𓇡.**"
